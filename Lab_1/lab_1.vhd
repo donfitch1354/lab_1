8ab592c2fce03378d0d08037cb2ffec35689118e
Ethan Snyder <ethan.snyder@usafa.edu>
2014-01-29 11:03:20 -0700

    initial commit

---
+++ Lab_1/lab_1.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:32:00 01/29/2014 
+-- Design Name: 
+-- Module Name:    lab_1 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lab_1 is
+end lab_1;
+
+architecture Behavioral of lab_1 is
+
+begin
+
+
+end Behavioral;
+
