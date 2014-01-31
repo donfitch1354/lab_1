08149b19c4ed34bb19c151d23f41dc36511a7860
Ethan Snyder <ethan.snyder@usafa.edu>
2014-01-31 12:55:06 -0700

    inital commit

---
+++ Lab_1/atlys_lab_video.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:44:25 01/31/2014 
+-- Design Name: 
+-- Module Name:    atlys_lab_video - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity atlys_lab_video is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           tmds : out  STD_LOGIC;
+           tmdsb : out  STD_LOGIC);
+end atlys_lab_video;
+
+architecture Behavioral of atlys_lab_video is
+
+begin
+
+
+end Behavioral;
+
---
+++ Lab_1/v_sync_gen.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:40:40 01/31/2014 
+-- Design Name: 
+-- Module Name:    v_sync_gen - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity v_sync_gen is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           h_completed : in  STD_LOGIC;
+           v_sync : out  STD_LOGIC;
+           blank : out  STD_LOGIC;
+           completed : out  STD_LOGIC;
+           row : out  STD_LOGIC);
+end v_sync_gen;
+
+architecture Behavioral of v_sync_gen is
+
+begin
+
+
+end Behavioral;
+
